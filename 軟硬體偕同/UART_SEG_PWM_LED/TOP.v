14c627a40bd791616f90c227816beb9a2962792b
Chi-Yung <105225932+Chi-Yung@users.noreply.github.com>
2023-12-18 01:15:09 +0800

    Add files via upload

---
+++ 軟硬體偕同/UART_SEG_PWM_LED/TOP.v
@@ -0,0 +1,20 @@
+module TOP(clk,reset);
+input clk;
+input reset;
+//input RX;
+wire [1:0]wRate;
+MODE_CONTROL(
+.clk(clk),
+.reset(reset),
+.idata(),
+.oSTART(),
+.orate_control(wRate)
+);
+DIV_Clk(
+.iClk(clk),   /* 100 Mhz clock */
+.iRSt_n(reset),
+.iRate_control(wRate),/* 重置訊號 低準位(0)動作 */
+.oClk1s(),       /* 1Hz pulse */
+);
+
+endmodule
