248c4e33395915d92ae2cd13ccf250fa48c9cc9a
Chi-Yung <105225932+Chi-Yung@users.noreply.github.com>
2024-01-07 19:52:53 +0800

    Update filelist.f

--- 軟硬體偕同/專題測試版本/filelist.f
+++ 軟硬體偕同/專題測試版本/filelist.f
@@ -1,1 +1,1 @@
-vlog uart_rx_tb.v MODE_CONTROL.v TOP.v uart_rx.v DIV_Clk.v fifo.v asc_to_dec.v scroller.v DEC2SEG.v TX_DATA_MEM.v UART_TX.v UART_TX_BAUD_GEN.v UART_RX_TX_FSM.v
+vlog uart_rx_tb.v MODE_CONTROL.v TOP.v uart_rx.v DIV_Clk.v fifo.v asc_to_dec.v scroller.v DEC2SEG.v TX_DATA_MEM.v UART_TX.v UART_TX_BAUD_GEN.v UART_TX_FSM.v
